62ee99657b5e7c31c6fff7637d2402d8fa6e34ee
Nallely <nallely232@hotmail.com>
2020-02-27 12:27:49 -0600

    Boton

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 
-console.log(example);
+//console.log(example);
 
+function showAndHide (){
+
+  let screen = document.getElementById("theme");
+  screen.style = "display:none";
+  document.getElementById('theme2').style.display ="block";
+}
+
+document.getElementById("meet").onclick = function() {showAndHide()};
