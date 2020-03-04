f32996fac7c5277e72140cc28417c70793c0e3c6
Nallely <nallely232@hotmail.com>
2020-03-04 11:41:21 -0600

    Filtros

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,10 @@
 //import { example } from './data.js';
 
 const showPokemons = document.getElementById('meet1');
+const _showAll = document.getElementById('showAll');
 const dataPokemon = window.POKEMON.pokemon;
 const root = document.getElementById('root');
+const elementType = document.getElementsByClassName('elementType')
 
 function showAndHide (){
   let screen = document.getElementById("_welcome");
@@ -15,12 +17,29 @@ function showAndHide (){
 const printData = (data) => {
   root.innerHTML = '';
   data.forEach(pokemon => {
-    let namePokemon = `<div class="tarjeta"><figure> <img = class "imageBox" src ="${pokemon.img}"> </figure>  ${pokemon.num} ${pokemon.name}     
+    let namePokemon = `<div class="tarjeta"><figure> <img = class "imageBox" src ="${pokemon.img}"> </figure>  ${pokemon.num} ${pokemon.name}
 <p> TYPE: ${pokemon.type} </p>
 `
     root.insertAdjacentHTML("beforeend", namePokemon);
   })
 }
+
+//funcion que imprime la data filtrada por elementos
+for (let i = 0; i < elementType.length; i++) {
+  elementType[i].addEventListener('click', () => {
+    let elementPokemon = elementType[i].id;
+    let pokemonType = window.pokemon.filterPokemons(dataPokemon, elementPokemon);
+    window.pokemon.ordenarPorNombre(pokemonType);
+    printData(pokemonType);
+
+
+  });
+
+
+}
 showPokemons.addEventListener("click",() => {
+   printData(dataPokemon)
+})
+_showAll.addEventListener("click",() => {
    printData(dataPokemon)
 })
