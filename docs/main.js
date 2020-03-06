317c392b2b933d4e5eca99257811ce974f5d0fe7
Nallely <nallely232@hotmail.com>
2020-03-05 18:30:26 -0600

    Docs

--- docs/main.js
+++ docs/main.js
@@ -1,45 +1,65 @@
-//import { example } from './data.js';
 
-const showPokemons = document.getElementById('meet1');
-const _showAll = document.getElementById('showAll');
-const dataPokemon = window.POKEMON.pokemon;
-const root = document.getElementById('root');
-const elementType = document.getElementsByClassName('elementType')
+import data from './data/pokemon/pokemon.js';
+import {filterType,sortbyName} from './data.js';
 
+
+// import {typeFilter} from './data.js';
+
+let root = document.querySelector('#root'); //dibujamos la tabla
+let elementType = document.getElementsByClassName('elementType')
+const dataPokemon = data.pokemon;
+
+//funcion llenado tarjeta pokemon
+function printData(dataPokemon){
+  root.innerHTML = '';
+  data.pokemon.forEach(pokemon =>{ //recorremos todos los productos
+   let namePokemon = `<div class="tarjeta"><figure> <img = class "imageBox" src ="${pokemon.img}"> </figure>  ${pokemon.num} ${pokemon.name}
+    <dt>TYPE: ${pokemon.type} </dt>
+    <dt>HEIGHT:${pokemon.height}</dt>
+    <dt>WEIGHT:${pokemon.weight}</dt>
+    <dt>CANDY:${pokemon.candy}</dt>
+    <dt>CANDY COUNT:${pokemon.candy_count}</dt>
+    <dt>EGG: ${pokemon.egg}</dt>
+    <dt>SPAWN CHANCE: ${pokemon.spawn_chance}</dt>
+    <dt>AVERANGE SPAWNS: ${pokemon.avg_spawns}</dt>
+    <dt>SPAWN TIME:${pokemon.spawn_time}</dt>
+    <dt>MULTIPLIERS:${pokemon.multipliers}</dt>
+    <dt>WEAKNESSES:${pokemon.weaknesses}</dt></div>`
+      root.insertAdjacentHTML("beforeend", namePokemon);
+ })}
+ document.getElementById("meet1").addEventListener('click',printData);
+//funcion filtros por tipo de elemento
+ function printData1(data){
+   root.innerHTML = '';
+   data.forEach(pokemon =>{ //recorremos todos los productos
+    let namePokemon = `<div class="tarjeta"><figure> <img = class "imageBox" src ="${pokemon.img}"> </figure>  ${pokemon.num} ${pokemon.name}
+     <dt>TYPE: ${pokemon.type} </dt>
+     <dt>HEIGHT:${pokemon.height}</dt>
+     <dt>WEIGHT:${pokemon.weight}</dt>
+     <dt>CANDY:${pokemon.candy}</dt>
+     <dt>CANDY COUNT:${pokemon.candy_count}</dt>
+     <dt>EGG: ${pokemon.egg}</dt>
+     <dt>SPAWN CHANCE: ${pokemon.spawn_chance}</dt>
+     <dt>AVERANGE SPAWNS: ${pokemon.avg_spawns}</dt>
+     <dt>SPAWN TIME:${pokemon.spawn_time}</dt>
+     <dt>MULTIPLIERS:${pokemon.multipliers}</dt>
+     <dt>WEAKNESSES:${pokemon.weaknesses}</dt></div>`
+     root.insertAdjacentHTML("beforeend", namePokemon);
+
+  })}
+for (let i = 0; i < elementType.length; i++) {
+  elementType[i].addEventListener('click', () => {
+    let elementPokemon = elementType[i].id;
+    let pokemonType = filterType(dataPokemon, elementPokemon);
+    let filter=sortbyName(pokemonType);
+     printData1(filter);
+  });
+}
+//funcion oculta pantalla de BIENVENIDA
 function showAndHide (){
   let screen = document.getElementById("_welcome");
   screen.style = "display:none";
   document.getElementById('_welcome1').style.display ="block";
 }
 
-//funcion que jala la data con la prepiedades que especifique
-
-const printData = (data) => {
-  root.innerHTML = '';
-  data.forEach(pokemon => {
-    let namePokemon = `<div class="tarjeta"><figure> <img = class "imageBox" src ="${pokemon.img}"> </figure>  ${pokemon.num} ${pokemon.name}
-<p> TYPE: ${pokemon.type} </p>
-`
-    root.insertAdjacentHTML("beforeend", namePokemon);
-  })
-}
-
-//funcion que imprime la data filtrada por elementos
-for (let i = 0; i < elementType.length; i++) {
-  elementType[i].addEventListener('click', () => {
-    let elementPokemon = elementType[i].id;
-    let pokemonType = window.pokemon.filterPokemons(dataPokemon, elementPokemon);
-    window.pokemon.ordenarPorNombre(pokemonType);
-    printData(pokemonType);
-
-
-  });
-
-
-}
-showPokemons.addEventListener("click",() => {
-   printData(dataPokemon)
-})
-_showAll.addEventListener("click",() => {
-   printData(dataPokemon)
-})
+document.getElementById("meet1").addEventListener('click',showAndHide);
